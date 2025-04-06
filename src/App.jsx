49c9a87a7a6632d5c00faf3bce969ab2299b7317
RifatParadoxical <zikkiz275@gmail.com>
2025-04-06 23:38:31 +0600

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-const [todo, setTodo] = useState(null)
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      let url = `http://localhost:3000/repoes`;
-      const res = await fetch(url)
-      if (!res.ok) {
-        throw new Error(`HTTP Error. status: ${res.status}`);
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+     const fetchData = async () => {
+       try {
+      const url = 'http://localhost:5173/repoes';
+      const res = await fetch(url);
+        const data = await res.json()
+        setTodo(data)
+      } catch (error) {
+        console.error(error)
       }
-      const post = await res.json()
-      setTodo(post)
-
-    } catch (error) {
-      console.error("Error fetching data:", error)
-    }
-  } 
-}, [todo])
-
+     }
+     fetchData()
+  }, []);
 
   return (
-      <div className='container'>
+    <div className="container">
+      
       {todo ? (
         Array.isArray(todo) ? (
-          todo.map((user, index) => <div key={index}><h2>{user.name || "Unnamed User"}</h2><p>{user.title}</p><button onClick={dlt}>Delete</button></div>)
+          todo.map((user, index) => <p key={index}>{user.name || "Unnamed User"}</p>)
         ) : (
           <p>{todo.name || "Unnamed User"}</p>
         )
       ) : (
         <p>Loading...</p>
       )}
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
